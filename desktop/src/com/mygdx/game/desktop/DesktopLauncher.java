aa448f18cc3d1517e439bbe5e15deb0c1f041313
Klemens <klemensle@edu.aau.at>
2017-04-27 17:12:01 +0200

    Test for jenkins

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -2,7 +2,7 @@ package com.mygdx.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.mygdx.game.Game;
+//import com.mygdx.game.Game;
 
 import GUI.Main;
 
