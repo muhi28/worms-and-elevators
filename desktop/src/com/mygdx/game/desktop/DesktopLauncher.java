e3bdb4d004bc6cd297779506524dec69f4d5251e
allenab <allenab@edu.aau.at>
2017-05-09 16:50:00 +0200

    Change dependency

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -3,15 +3,15 @@ package com.mygdx.game.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
-import core.assets.assets.GUI.Main;
+//import core.assets.assets.GUI.Main;
 
 //import com.mygdx.game.Game;
 
 
 public class DesktopLauncher {
 
-    public static void main(String[] arg) {
-        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-        new LwjglApplication(new Main("blau"), config);
-    }
+//    public static void main(String[] arg) {
+//        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+//        new LwjglApplication(new Main("blau"), config);
+//    }
 }
