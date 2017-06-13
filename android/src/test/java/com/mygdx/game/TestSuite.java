34232d4f7359f7cdb6e2dae6ebae488d3ca962f7
Muhi <msiljic28@hotmail.com>
2017-06-13 14:54:27 +0200

    Created new branch for design changes.

--- android/src/test/java/com/mygdx/game/TestSuite.java
+++ android/src/test/java/com/mygdx/game/TestSuite.java
@@ -4,7 +4,9 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 /**
- *Created by Muhi on 10.05.2017.
+ * Test Suite.
+ *
+ * @author Muhamed Siljic
  */
 @RunWith(Suite.class)
 @Suite.SuiteClasses({DiceTest.class,FieldTest.class, ElevatorTest.class, PlayerTest.class})
